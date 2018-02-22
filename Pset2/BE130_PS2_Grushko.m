76be06ba5ecc074b6e5874771aabade69bdad432
Mikhail Grushko <grushko@college.harvard.edu>
2018-02-21 20:44:28 -0500

    restored some

--- Pset2/BE130_PS2_Grushko.m
+++ Pset2/BE130_PS2_Grushko.m
@@ -28,4 +28,27 @@ for i = 1 : size
     PV(2, i) = r100(i)*sin(thetapref(i));
 end
 
+figure;
 plotv(PV);
+
+% 100 evenly spaced
+
+size = 100;
+r100 = zeros(1, size);
+thetapref = 2*pi*linspace(0, 1, size);
+
+
+for i = 1 : length(r100)
+    r100(i) = k1 * cos(thetapref(i) - theta) + k0;
+end
+
+PV = zeros(2, 100);
+
+for i = 1 : size
+    PV(1, i) = r100(i)*cos(thetapref(i));
+    PV(2, i) = r100(i)*sin(thetapref(i));
+end
+
+figure;
+plotv(PV);
+
